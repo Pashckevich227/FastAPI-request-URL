e506d179953058d70da86264c8a98777ed648624
Pashckevich227 <iport234@gmail.com>
2023-05-03 20:36:27 +0300

    modufied code

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from typing import Optional
 
 app = FastAPI()
 
+# Подключаем путь до шаблона
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 
 
-@app.get("/items/{name}", response_class=HTMLResponse)
-async def read_item(request: Request, name: str):
-    return templates.TemplateResponse("iter.html", {"request": request, "name": name})
-
+# Функция обработки URL по get запросу
 @app.get("/", response_class=HTMLResponse)
 async def hello(request: Request, name: Optional[str] = None, message: Optional[str] = None):
     error = {"Error": "Name or message field is required"}
